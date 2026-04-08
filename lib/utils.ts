64660fb3c3602d5d00204a543bc7654cbe570c3d
quangnguyen11111111 <quangnguyen1507204@gmail.com>
2026-04-08 23:46:13 +0700

    create an HTTP client to call the API

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Xoá đi kí tự `/` đầu tiên của path
+export const normalizePath = (path: string) => {
+  return path.startsWith('/') ? path.slice(1) : path
+}
